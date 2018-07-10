320b025af045e46fa0fa33c7f4752c9ab09f74ec
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-07-10 19:21:21 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
-using namespace std;
 #include"Header.h"
 
 
 int main()
 {
+	query q;
 	wordTrie fuck;
+
 	string query;
 	fuck.add("data\\1.txt");
-	//fuck.print();
 	cout << "Enter:";
-	cin >> query;
-	if (fuck.search(query))cout << "YES" << endl;
-	else cout << "NO" << endl;
-	//system("pause");
+	getline(cin,query);
+	q.insert_query(query, fuck);
+	//if (fuck.search(query))cout << "YES" << endl;
+	//else cout << "NO" << endl;
+	
 	return 0;
 }
