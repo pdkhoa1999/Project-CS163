bfe45e6a44696f8e2519be8c682ee3601505e8f3
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-07-10 22:35:09 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,25 @@
 
 int main()
 {
+
+	clock_t start = clock();
 	query q;
 	wordTrie fuck;
-
+	string path = "data\\";
+	for (auto & p : std::experimental::filesystem::directory_iterator(path)) {
+		string p2 = p.path().string();
+		cout << p2 << endl;
+		fuck.add(p2);
+	}
+	fuck.savewordFile();
 	string query;
-	fuck.add("data\\1.txt");
+	cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << endl;
 	cout << "Enter:";
 	getline(cin,query);
 	q.insert_query(query, fuck);
-	//if (fuck.search(query))cout << "YES" << endl;
-	//else cout << "NO" << endl;
-	
+	if (fuck.search(query))cout << "YES" << endl;
+	else cout << "NO" << endl;
+	clock_t finish = clock();
+	cout << finish - start << endl;
 	return 0;
 }
