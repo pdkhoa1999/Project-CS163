c71b720d97a8f916469541654158ef17309f7e71
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-07-10 22:34:16 +0700

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,24 +1,23 @@
 #include "Header.h"
-
 void wordTrie::add(string path)
 {
 	ifstream fin;
 	fin.open(path);
 	if (fin)
 	{
-		wordNode*cur = NULL;
 		while (!fin.eof())
 		{
 			wordNode* cur = NULL;
 			int n = 0;
 			string word = takeWord(fin, n);
+
 			if (root == NULL) root = new wordNode;
 			cur = root;
 			for (int i = 0; i < n; i++)
 			{
 
 				char x = tolower(word[i]);
-				if (x - 'a' >= 26 || x - 'a' < 0)
+				if (x - 'a' >= 26 || x - 'a' < 0)//if not alphabet ->break
 					break;
 				if (cur->children[x - 'a'] == NULL)
 				{
@@ -26,30 +25,33 @@ void wordTrie::add(string path)
 				}
 				cur = cur->children[x - 'a'];
 			}
-			if (cur->phead == NULL)
+			if (cur->phead == NULL)//create first data node for the word
 			{
 				cur->phead = new pathNode;
 				cur->phead->occurance++;
 				cur->phead->path = path;
 				cur->phead->pnext = NULL;
+				cur->n++;
 			}
 			else
 			{
 				pathNode*curpath = cur->phead;
-				while (curpath->pnext != NULL&&curpath->path != path)
+				while (curpath->pnext != NULL&&curpath->path != path)//create nodes after the first node
 				{
 					curpath = curpath->pnext;
 				}
-				if (curpath->path == path)
+				if (curpath->path == path)//the file already in the data
 				{
 					curpath->occurance++;
 				}
 				else
 				{
-					curpath->pnext = new pathNode;
+					curpath->pnext = new pathNode;//new file
 					curpath = curpath->pnext;
 					curpath->path = path;
 					curpath->occurance++;
+					curpath->pnext = NULL;
+					cur->n++;
 				}
 
 			}
@@ -69,7 +71,7 @@ string wordTrie::takeWord(ifstream &fin, int &n)
 	string word, word2, word3;
 	char x;
 	fin >> x;
-	while (!checkValidation(x))fin >> x;
+	while (!checkValidation(x) && fin)fin >> x;
 	word += x;
 	getline(fin, word2, ' ');
 	word3 = word + word2;
@@ -82,7 +84,7 @@ void wordTrie::printInternal(wordNode*root, string word)
 {
 	if (root->phead != NULL)
 	{
-		cout << word << "-";
+		cout << word << "-" << root->n << "-";
 		pathNode *cur = root->phead;
 		while (cur != NULL)
 		{
@@ -106,6 +108,99 @@ void wordTrie::print()
 	string word;
 	printInternal(root, word);
 }
+void wordTrie::loadwordFile(string path)
+{
+	ifstream fin;
+	fin.open(path);
+	if (fin)
+	{
+		while (!fin.eof())
+		{
+			string word;
+			getline(fin, word, '-');
+			wordNode* cur = NULL;
+			int n = word.length();
+			if (root == NULL) root = new wordNode;
+			cur = root;
+			for (int i = 0; i < n; i++)
+			{
+
+				char x = tolower(word[i]);
+				if (x - 'a' >= 26 || x - 'a' < 0)//if not alphabet ->break
+					break;
+				if (cur->children[x - 'a'] == NULL)
+				{
+					cur->children[x - 'a'] = new wordNode;
+				}
+				cur = cur->children[x - 'a'];
+			}
+			fin >> cur->n;
+			pathNode*curpath = NULL;
+			for (int i = 0; i < cur->n; i++)
+			{
+				int x;
+				fin.ignore();
+				getline(fin, path, ',');
+				fin >> x;
+				if (cur->phead == NULL)//create first data node for the word
+				{
+					cur->phead = new pathNode;
+					curpath = cur->phead;
+					cur->phead->occurance += x;
+					cur->phead->path = path;
+					cur->phead->pnext = NULL;
+				}
+				else
+				{
+					curpath->pnext = new pathNode;
+					curpath = curpath->pnext;
+					curpath->occurance += x;
+					curpath->path = path;
+					curpath->pnext = NULL;
+
+
+				}
+			}
+			fin.ignore(2);
+		}
+	}
+	fin.close();
+}
+void wordTrie::saveInternal(ofstream &fout, wordNode*root, string word) {
+
+	if (fout)
+	{
+		if (root->phead != NULL)
+		{
+			fout << word << "-" << root->n << "-";
+			pathNode*cur = root->phead;
+			while (cur != NULL)
+			{
+				fout << cur->path << "," << cur->occurance << ";";
+				cur = cur->pnext;
+			}
+			fout << endl;
+		}
+		for (int i = 0; i < 26; i++)
+		{
+			if (root->children[i] != NULL)
+			{
+				word += 'a' + i;
+				saveInternal(fout, root->children[i], word);
+				word.pop_back();
+			}
+		}
+	}
+}
+void wordTrie::savewordFile()
+{
+	ofstream fout;
+	fout.open("wordFile.txt");
+	string m;
+	saveInternal(fout, root, m);
+	fout.close();
+}
+
 bool wordTrie::search(string s, wordNode *root)
 {
 	int i = 0;
