502dd866f3f0321a5f8a59fa401039281c2d701c
demdecuong <32518096+demdecuong@users.noreply.github.com>
2018-07-19 21:55:35 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,21 +6,52 @@ int main()
 
 	clock_t start = clock();
 	query q;
+	StopWordChaining stopword;
 	wordTrie fuck;
+	stopword.RuncreateChaining();    // Load wordFile
 	string path = "data\\";
 	for (auto & p : std::experimental::filesystem::directory_iterator(path)) {
 		string p2 = p.path().string();
-		cout << p2 << endl;
+	//	cout << p2 << endl;
 		fuck.add(p2);
 	}
 	fuck.savewordFile();
 	string query;
 	cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << endl;
-	cout << "Enter:";
-	getline(cin,query);
-	q.insert_query(query, fuck);
-	if (fuck.search(query))cout << "YES" << endl;
-	else cout << "NO" << endl;
+	while (query != "0")
+	{
+		cout << "Enter:";
+		getline(cin, query);
+		q.load_Query(query);
+		//if (stopword.isStopWord(query)) cout << "good!";
+		//else cout << "fail!";
+		cout << "Position to add:";
+		string s1;
+		int n;
+		cin >> n;
+		cout << "String to add:";
+		cin.ignore();
+		getline(cin, s1, '\n');	
+		q.insert_Query(s1, n);
+		q.ShowPrint();
+		cout << endl;
+		cout << "String to remove:";
+		string s;
+		getline(cin, s,'\n');
+		q.remove_Query(s);
+		q.ShowPrint();
+		cout << endl;
+	}
+
+	/*while (query != "0") {
+		q.insert_query(query, fuck,stopword);
+		cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << endl;
+		cout << "Enter:";
+		getline(cin, query);
+	}*/
+	
+	//if (fuck.search(query))cout << "YES" << endl;
+	//else cout << "NO" << endl;
 	clock_t finish = clock();
 	cout << finish - start << endl;
 	return 0;
